78d43e04808c5439a59167bf436177641f065664
Ethan Sewall, the Crab Keeper <s209066@students.aie.edu>
2020-10-02 10:17:15 -0700

    initial commit

---
+++ MathClasses/Vector3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClasses
+{
+    public class Vector3
+    {
+        public float x, y, z;
+
+        public Vector3()
+        {
+            x = 0;
+            y = 0;
+            z = 0;
+        }
+
+        public Vector3(float x1, float y1, float z1)
+        {
+            x = x1;
+            y = y1;
+            z = z1;
+        }
+
+        public static Vector3 operator +(Vector3 v1, Vector3 v2)
+        {
+            return new Vector3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+        }
+
+        public static Vector3 operator -(Vector3 v1, Vector3 v2)
+        {
+            return new Vector3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+        }
+    }
+}
