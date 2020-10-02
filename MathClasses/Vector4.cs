7438e600304925f7ff7964f453786eeac817ebf8
Ethan Sewall, the Crab Keeper <s209066@students.aie.edu>
2020-10-02 11:22:49 -0700

    vector4 constructor, +/- operator

--- MathClasses/Vector4.cs
+++ MathClasses/Vector4.cs
@@ -18,5 +18,23 @@ namespace MathClasses
             w = 0;
         }
 
+        public Vector4(float x1, float y1, float z1, float w1)
+        {
+            x = x1;
+            y = y1;
+            z = z1;
+            w = w1;
+        }
+
+        public static Vector4 operator +(Vector4 v1, Vector4 v2)
+        {
+            return new Vector4(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z, v1.w + v2.w);
+        }
+
+        public static Vector4 operator -(Vector4 v1, Vector4 v2)
+        {
+            return new Vector4(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z, v1.w + v2.w);
+        }
+
     }
 }
